1609e84dba7b2d2d4786ee740dc8c9587416847a
Acunnia <carlosacunagg@gmail.com>
2021-01-06 18:10:18 +0100

    new heroku version

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(__dirname+'/dist/cabina-bailon'));
+app.get('/*',function(req,res){
+  res.sendFile('index.html', {root: 'dist/cabina-bailon/'});
+});
+
+app.listen(process.env.PORT || 8080);
